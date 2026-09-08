f4aba347a79f35b0984093a333c144bf2689fae0
GAO MINGYANG <kosakagmy@icloud.com>
2026-09-08 22:35:58 +0900

    Add Hello World test project

---
+++ Test_1/Test_1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Test_1
+//
+//  Created by GAO MINGYANG on 2026/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "globe")
+            if true {
+                Text("Hello, World!")
+            }
+            Text("howdy")
+            Circle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_1/Test_1/Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_1App.swift
+//  Test_1
+//
+//  Created by GAO MINGYANG on 2026/09/08.
+//
+
+import SwiftUI
+
+@main
+struct Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
